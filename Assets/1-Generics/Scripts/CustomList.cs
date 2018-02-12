e1da3d689f8ac5772932feeb83bd3d78773a94c4
archrofe <archrofe@gmail.com>
2018-02-12 16:02:12 +1100

    GenericsTest & CustomList Script

---
+++ Assets/1-Generics/Scripts/CustomList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generics
+{
+    public class CustomList<T>
+    {
+        private int amount = 0;
+        private T[] data;
+
+        public CustomList() { amount = 0; }
+        public void Add(T item)
+        {
+            // Create a new array of amount + 1
+            T[] cache = new T[amount + 1];
+            // Create if the list has been initialised
+            if (data != null)
+            {
+                // Copy all existing items to new array
+                for (int i = 0; i < data.Length; i++)
+                {
+                    // Copy each element to cache
+                    cache[i] = data[i];
+                }
+            }
+            // Set the end element to new item
+            cache[amount] = item;
+            // Point to new array
+            data = cache;
+            // Increment amount
+            amount++;
+        }
+    }
+}
